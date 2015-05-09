76f8f4c32751053d8994c1138907cde6e8fe6c5b
kenji-special <k.saito.1985@gmail.com>
2015-05-10 01:12:35 +0900

    back to before.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var express = require('express')
+var express = require('express');
 var MobileDetect = require('./mobile-detect');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  //response.send('Hello World!')
-  response.sendFile(__dirname + "/public/pc/index.html");
+
+  var md = new MobileDetect(request.headers['user-agent']);
+  if(md.mobile()){
+    response.sendFile(__dirname + "/public/sp/index.html");
+  }else{
+    response.sendFile(__dirname + "/public/pc/index.html");
+  }
+
 })
 
 app.listen(app.get('port'), function() {
