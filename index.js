62e6f7e5524282728ca3ff937986c93d0db13014
kenji-special <k.saito.1985@gmail.com>
2015-05-10 01:05:33 +0900

    back to  initialize.

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var MobileDetect = require('mobile-detect');
+var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var md = new MobileDetect(request.headers['user-agent']);
-  if(md.mobile()){
-    response.sendFile(__dirname + "/public/sp/index.html");
-  }else{
-    response.sendFile(__dirname + "/public/pc/index.html");
-  }
-
-});
+  response.send('Hello World!')
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-});
+})
